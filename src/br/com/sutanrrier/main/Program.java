74e9dba5c21761e5f0992c5debb7d939e5d09637
Sutanrrier <pabloryanbr0@gmail.com>
2022-05-02 16:19:43 -0300

    Carro and Estacionamento class created

---
+++ src/br/com/sutanrrier/main/Program.java
@@ -0,0 +1,12 @@
+package br.com.sutanrrier.main;
+
+import br.com.sutanrrier.model.entities.Carro;
+import br.com.sutanrrier.model.entities.Estacionamento;
+
+public class Program {
+	public static void main(String[] args) {
+		Carro carro1 = new Carro();
+		Estacionamento estacionamento = new Estacionamento();
+		
+	}
+}
